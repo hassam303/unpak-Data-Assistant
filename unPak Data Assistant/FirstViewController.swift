e093b163160ea5c93b502f21fcbb341f6cc9d3b5
Hassam Solano <solanomorelhs@g.cofc.edu>
2015-05-15 20:39:32 -0400

    Enabled list gathering from dropbox

--- unPak Data Assistant/FirstViewController.swift
+++ unPak Data Assistant/FirstViewController.swift
@@ -8,27 +8,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,DBRestClientDelegate,UITableViewDataSource,UITableViewDelegate {
+	
+	@IBOutlet weak var tableView: UITableView!
+	
+	var restClient:DBRestClient?
+	var availableFormsArray:[String] = Array<String>()
+	
+	
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+		self.restClient = DBRestClient(session: DBSession.sharedSession())
+		self.restClient!.delegate = self
+		self.restClient!.loadMetadata("/OpenForms")
+		
+		self.tableView.dataSource = self
+
+		
+		
 	}
 
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
 	}
-
-
+	
+	
+	
+	//UI Related Actions
 	@IBAction func signInWasPressed(sender: AnyObject) {
 		
 		   DBSession.sharedSession().linkFromController(self)
 		
 	}
 	
-	
-	@IBAction func didRefresh(sender: AnyObject) {
+	@IBAction func chooserSelected(sender: AnyObject) {
 		
 		DBChooser.defaultChooser().openChooserForLinkType(DBChooserLinkTypePreview, fromViewController: self, completion: { (results: [AnyObject]!) -> Void in
 			
@@ -37,33 +52,48 @@ class ViewController: UIViewController {
 				var dbResult:DBChooserResult = results [0] as! DBChooserResult
 				
 				println(dbResult.link)
-
-			}
-			
-			else {
 				
+			}
+				
+			else {
 				println("User canceled selection")
-	
 			}
 		})
-	
 		
+	}
+	
+	@IBAction func refreshButtonPressed(sender: AnyObject) {
+		tableView.reloadData()
+	}
+	
+	
+	//RestClient delegate actions
+	func restClient(client: DBRestClient!, loadedMetadata metadata: DBMetadata!) {
+		if metadata.isDirectory{
+			for item in metadata.contents{
+				println(item.filename!!)
+				availableFormsArray.append(item.filename!!)
+			}
+		}
+	}
+	
+	
+	//UITableView Set-Up
+	func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+		var cell:UITableViewCell = tableView.dequeueReusableCellWithIdentifier("openFormsCell") as! UITableViewCell
+		let iP = indexPath
+		cell.textLabel!.text = availableFormsArray[iP.row]
 		
-		
-}
+		return cell
+	}
 	
+	func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return self.availableFormsArray.count
+	}
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+		return 1
+	}
 	
 	
 }

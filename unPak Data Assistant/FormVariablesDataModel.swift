96e2abb9902a108f0eaf9e17fc6b0e7928fc7443
Hassam Solano <solanomorelhs@g.cofc.edu>
2015-05-26 14:00:28 -0400

    Continued configuration of core data: add classFiles to match entities in database.

---
+++ unPak Data Assistant/FormVariablesDataModel.swift
@@ -0,0 +1,17 @@
+//
+//  FormVariablesDataModel.swift
+//  unPak Data Assistant
+//
+//  Created by Hassam Solano on 5/26/15.
+//  Copyright (c) 2015 Hassam Solano. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class FormVariablesDataModel: NSManagedObject {
+	@NSManaged	var plantId: String
+	@NSManaged	var plantPosition: String
+	
+   
+}
